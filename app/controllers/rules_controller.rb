fbe6843468ca29ddccaac5d583e3efe36cacc596
Don Kelly <karfai@gmail.com>
2016-07-15 00:42:36 -0400

    In RulesController, implement associated destroy

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,7 +1,25 @@
 class RulesController < ApplicationController
   respond_to :json
+
+  before_filter :lookup_rule
+  before_filter :maybe_lookup_account
   
   def destroy
+    if @account
+      Rails.logger.info("Disassociating (account=#{@account.id}; rule=#{@rule.id})")
+      @account.rules.delete(@rule)
+    end
     render(nothing: true, status: :ok)
+  end
+
+  private
+
+  def lookup_rule
+    @rule = Rule.find(params['id'])
+  end
+
+  def maybe_lookup_account
+    params.fetch('account_id', nil)
+    @account = Account.find(account_id) if account_id
   end
 end
