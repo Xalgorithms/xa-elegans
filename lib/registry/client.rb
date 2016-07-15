405468a2824edd60cecb9398d831d9d888db0e50
Don Kelly <karfai@gmail.com>
2016-07-15 00:42:49 -0400

    Pull Registry client from xa-repository.simple and extend

---
+++ lib/registry/client.rb
@@ -0,0 +1,60 @@
+module Registry
+  # this came from xa-repository.simple it should be factored into a
+  # gem that can be shared
+  class Client
+    def initialize
+      @conn = Faraday.new(registry_url) do |f|
+        f.request(:url_encoded)
+        f.request(:json)
+        f.response(:json, :content_type => /\bjson$/)
+        f.adapter(Faraday.default_adapter)        
+      end
+    end
+
+    def registry_url
+      ENV.fetch('XA_REGISTRY_URL', 'http://localhost:3000')
+    end
+    
+    def update_rule(name, version, repo_id)
+      puts "> PUT /api/v1/rules/#{name}/#{version}"
+      resp = @conn.put("/api/v1/rules/#{name}", rule: { version: version, repository: { id: repo_id } })
+      puts "< #{resp.status}"
+      if resp.success?
+        yield(resp.body.fetch('public_id'))
+      end
+    end
+
+    def register(url)
+      puts "> POST /api/v1/repositories"
+      resp = @conn.post('/api/v1/repositories', repository: { url: url })
+      puts "< #{resp.status}"
+      if resp.success?
+        yield(resp.body.fetch('public_id'))
+      end
+    end
+
+    def rules(&bl)
+      get('/api/v1/rules') do |rules|
+        bl.call(rules) if bl
+      end
+    end
+
+    private
+    
+    def get(relative_url, &bl)
+      logger.info("> GET #{relative_url}")
+      resp = @conn.get(relative_url)
+      logger.info("< #{resp.status}")
+      if resp.success?
+        bl.call(resp.body) if bl
+        resp.body
+      else
+        nil
+      end
+    end
+
+    def logger
+      Rails.logger
+    end
+  end
+end
