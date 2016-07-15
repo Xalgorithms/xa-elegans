461ca70d4945b54badb5d320ee2f9c22cbb16f4d
Don Kelly <karfai@gmail.com>
2016-07-15 00:42:33 -0400

    add an account model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926151309) do
+ActiveRecord::Schema.define(version: 20150927153539) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
