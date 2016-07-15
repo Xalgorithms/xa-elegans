83c72a67fe253f92ff4b471e02a885b0005fd6ef
Don Kelly <karfai@gmail.com>
2016-07-15 00:42:40 -0400

    seed everything for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,54 +15,52 @@
   o.merge(kv.first => Rule.create(name: kv.first, parameters: parameters))
 end
   
-if Rails.env.development?
-  fuel = {
-    'bob@nowhere.com' => {
-      :full_name   => 'Bob Smith',
-      :password    => 'password',
-      :accounts    => {
-        'Transfer to Yerevan' => [
-          {
-            :rule       => 'cost-of-living',
-            :assignments => {
-              'from' => '{ "city" : "Ottawa", "country" : "Canada" }',
-              'to'   => '{ "city" : "Yerevan", "country" : "Armenia" }',
-            },
+fuel = {
+  'bob@nowhere.com' => {
+    :full_name   => 'Bob Smith',
+    :password    => 'password',
+    :accounts    => {
+      'Transfer to Yerevan' => [
+        {
+          :rule       => 'cost-of-living',
+          :assignments => {
+            'from' => '{ "city" : "Ottawa", "country" : "Canada" }',
+            'to'   => '{ "city" : "Yerevan", "country" : "Armenia" }',
           },
-          {
-            :rule       => 'currency-conversion',
-            :assignments => {
-              'from' => '{ "code" : "CAN" }',
-              'to'   => '{ "code" : "ARM" }',
-            },
+        },
+        {
+          :rule       => 'currency-conversion',
+          :assignments => {
+            'from' => '{ "code" : "CAN" }',
+            'to'   => '{ "code" : "ARM" }',
           },
-        ],
-        'Take some tax' => [
-          {
-            :rule       => 'witholding',
-            :assignments => {
-              'percentage' => '34',
-            },
-          }
-        ]
-      },
+        },
+      ],
+      'Take some tax' => [
+        {
+          :rule       => 'witholding',
+          :assignments => {
+            'percentage' => '34',
+          },
+        }
+      ]
     },
-  }
+  },
+}
 
-  fuel.each do |email, user_props|
-    if !User.find_by(email: email)
-      u = User.create(user_props.except(:accounts).merge(email: email))
-      user_props[:accounts].each do |name, invocations_props|
-        invocations = invocations_props.map do |invocation_props|
-          rule = Rule.find_by(name: invocation_props[:rule])
-          assignments = invocation_props[:assignments].map do |name, actual|
-            param = rule.parameters.where(name: name).first
-            Assignment.create(actual: actual, parameter: param)
-          end
-          Invocation.create(rule: rule, assignments: assignments)
+fuel.each do |email, user_props|
+  if !User.find_by(email: email)
+    u = User.create(user_props.except(:accounts).merge(email: email))
+    user_props[:accounts].each do |name, invocations_props|
+      invocations = invocations_props.map do |invocation_props|
+        rule = Rule.find_by(name: invocation_props[:rule])
+        assignments = invocation_props[:assignments].map do |name, actual|
+          param = rule.parameters.where(name: name).first
+          Assignment.create(actual: actual, parameter: param)
         end
-        Account.create(name: name, user: u, invocations: invocations)
+        Invocation.create(rule: rule, assignments: assignments)
       end
+      Account.create(name: name, user: u, invocations: invocations)
     end
   end
 end
