7b6b0756ce2fed982ec507adbd508aa0d724a8ab
Don Kelly <karfai@gmail.com>
2016-07-15 00:42:41 -0400

    Redo for JSON

--- parse.rb
+++ parse.rb
@@ -1,229 +1,217 @@
-require 'countries'
-require 'currencies'
 require 'nokogiri'
-require 'ostruct'
 
-class NodeWrapper
-  NAMESPACE_URNS = {
-    invoice: 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
-    cac:     'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2',
-    cbc:     'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
-  }
+module UBL
+  module Parse
+    def parse(urn)
+      load(urn) do |doc|
+        invoice = {}.tap do |o|
+          maybe_find_one(doc, "#{ns(doc, :invoice)}:Invoice") do |invoice_el|
+            maybe_find_one_text(invoice_el, "#{ns(doc, :cbc)}:ID") do |text|
+              o[:id] = text
+            end
+            maybe_find_one_text(invoice_el, "#{ns(doc, :cbc)}:IssueDate") do |text|
+              o[:issued] = text
+            end
+            maybe_find_one_text(invoice_el, "#{ns(doc, :cbc)}:DocumentCurrencyCode") do |text|
+            o[:currency] = text
+            end
+            
+            maybe_find_period(invoice_el) do |period|
+              o[:period] = period
+            end
+            
+            maybe_find_parties(invoice_el) do |parties|
+              o[:parties] = parties
+            end
+          end
+        end
+        
+        yield(invoice)
+      end
+    end
 
-  def self.maybe_make_one(xp, pn)
-    n = pn.xpath(xp).first if pn
-    o = self.new(n) if n
-    yield(o) if o
-    o
-  end
-
-  def maybe_find_one(xp, pn = nil, &bl)
-    rv = (pn || @node).xpath(xp).first
-    rv = bl.call(rv) if rv && bl
-    rv
+    def load(urn)
+      doc = Nokogiri::XML(open(urn)) do |cfg|
+        cfg.noblanks.noent
+      end
+      yield(doc) if doc
+    end
+    
+    def ns(n, k)
+      if !@nses
+        @namespace_urns ||= {
+          invoice: 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
+          cac:     'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2',
+          cbc:     'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
+        }
+        doc_nses = n.document.namespaces.invert
+        @nses = @namespace_urns.keys.inject({}) do |o, k|
+          o.merge(k => doc_nses[@namespace_urns[k]].split(':').last)
+        end
+      end
+      
+      @nses[k]
+    end
   end
   
-  def maybe_find_one_text(xp, pn = nil, &bl)
-    maybe_find_one(xp, pn) do |n|
-      rv = n.text
-      rv = bl.call(rv) if bl
-      rv
-    end
-  end
-
-  def maybe_find_one_date(xp, pn = nil, &bl)
-    maybe_find_one_text(xp, pn) do |text|
-      rv = Date.parse(text)
-      rv = bl.call(rv) if bl
-      rv
-    end
-  end
-
-  def maybe_find_one_currency(xp, pn = nil, &bl)
-    maybe_find_one_text(xp, pn) do |text|
-      # this does not check that the gc list matches ISO4217
-      rv = ISO4217::Currency.from_code(text)
+  module Maybes
+    def maybe_find_one(pn, xp, &bl)
+      rv = pn.xpath(xp).first
       rv = bl.call(rv) if rv && bl
       rv
     end
-  end
 
-  def maybe_find_one_country(xp, pn = nil, &bl)
-    maybe_find_one_text(xp, pn) do |text|
-      rv = ISO3166::Country.new(text)
-      rv = bl.call(rv) if bl
+    def maybe_find_set(pn, xp_set, &bl)
+      rv = xp_set.keys.inject({}) do |o, k|
+        maybe_find_one(pn, xp_set[k]) do |n|
+          o = o.merge(k => n)
+        end
+
+        o
+      end
+      rv = bl.call(rv) if rv.any? && bl
       rv
     end
-  end
-  
-  def initialize(n)
-    @node = n
-    doc_nses = @node.document.namespaces.invert
-    @nses = NAMESPACE_URNS.keys.inject({}) do |o, k|
-      o.merge(k => doc_nses[NAMESPACE_URNS[k]].split(':').last)
+
+    def maybe_find_set_text(pn, xp_set, &bl)
+      rv = {}
+      maybe_find_set(pn, xp_set) do |s|
+        rv = s.inject({}) do |o, kv|
+          o.merge(kv.first => kv.last.text)
+        end
+      end
+
+      rv = bl.call(rv) if rv.any? && bl
+      rv
     end
-  end
-end
-
-class Invoice < NodeWrapper
-  def id
-    @id ||= maybe_find_one_text("#{@nses[:invoice]}:Invoice/#{@nses[:cbc]}:ID")
-  end
-
-  def issued
-    @issued ||= maybe_find_one_date("#{@nses[:invoice]}:Invoice/#{@nses[:cbc]}:IssueDate")
-  end
-
-  def currency
-    @currency ||= maybe_find_one_currency("#{@nses[:invoice]}:Invoice/#{@nses[:cbc]}:DocumentCurrencyCode")
-  end
-
-  def period
-    maybe_find_one("#{@nses[:invoice]}:Invoice/#{@nses[:cac]}:InvoicePeriod") do |pn|
-      @period = OpenStruct.new(
-        {}.tap do |o|
-          maybe_find_one_date("#{@nses[:cbc]}:StartDate", pn) do |dt|
-            o[:starts] = dt
-          end
-          maybe_find_one_date("#{@nses[:cbc]}:EndDate", pn) do |dt|
-            o[:ends] = dt
-          end
-        end)
-    end if !@period
-
-    @period
-  end
-
-  def parties
-    maybe_find_one("#{@nses[:invoice]}:Invoice") do |pn|
-      @parties = OpenStruct.new(
-        {}.tap do |o|
-          maybe_find_one_party("#{@nses[:cac]}:AccountingSupplierParty/#{@nses[:cac]}:Party", pn) do |pt|
-            o[:supplier] = pt
-          end
-          maybe_find_one_party("#{@nses[:cac]}:AccountingCustomerParty/#{@nses[:cac]}:Party", pn) do |pt|
-            o[:customer] = pt
-          end
-          maybe_find_one_party("#{@nses[:cac]}:PayeeParty", pn) do |pt|
-            o[:payer] = pt
-          end
-        end)
-    end if !@parties
     
-    @parties
-  end
-
-  private
-
-  def maybe_find_one_party(xp, pn = nil)
-    maybe_find_one(xp, pn) do |n|
-      pt = party(n)
-      yield(pt)
-      pt
+    def maybe_find_one_text(pn, xp, &bl)
+      maybe_find_one(pn, xp) do |n|
+        rv = n.text
+        rv = bl.call(rv) if bl
+        rv
+      end
+    end
+    
+    def maybe_find_one_convert(sym, pn, xp, &bl)
+      maybe_find_one(pn, xp) do |n|
+        rv = send(sym, n)
+        rv = bl.call(rv) if rv && rv.any? && bl
+        rv
+      end
     end
   end
   
-  def party(n)
+  module Invoice
+    def maybe_find_period(invoice_el, &bl)
+      period_set = {
+        starts: "#{ns(invoice_el, :cac)}:InvoicePeriod/#{ns(invoice_el, :cbc)}:StartDate",
+        ends:   "#{ns(invoice_el, :cac)}:InvoicePeriod/#{ns(invoice_el, :cbc)}:EndDate",
+      }
+      
+      maybe_find_set_text(invoice_el, period_set) do |s|
+        bl.call(s) if bl
+      end
+    end
+    
+    def maybe_find_parties(invoice_el, &bl)
+      parties_set = {
+        supplier: "#{ns(invoice_el, :cac)}:AccountingSupplierParty/#{ns(invoice_el, :cac)}:Party",
+        customer: "#{ns(invoice_el, :cac)}:AccountingCustomerParty/#{ns(invoice_el, :cac)}:Party",
+        payer:    "#{ns(invoice_el, :cac)}:PayeeParty",
+      }
+      maybe_find_set(invoice_el, parties_set) do |parties_els|
+        parties = parties_els.inject({}) do |o, kv|
+          o.merge(kv.first => make_party(kv.last))
+        end
+        bl.call(parties) if parties.any? && bl
+      end
+    end
+    
+    def make_party(party_el)
     # ignoring: cbc:EndpointID, cbc:ID
-    OpenStruct.new(
       {}.tap do |o|
-        maybe_find_one_text("#{@nses[:cac]}:PartyIdentification/#{@nses[:cbc]}:ID", n) do |text|
+        maybe_find_one_text(party_el, "#{ns(party_el, :cac)}:PartyIdentification/#{ns(party_el, :cbc)}:ID") do |text|
           o[:id] = text
         end
-        maybe_find_one_text("#{@nses[:cac]}:PartyName/#{@nses[:cbc]}:Name", n) do |text|
+        maybe_find_one_text(party_el, "#{ns(party_el, :cac)}:PartyName/#{ns(party_el, :cbc)}:Name") do |text|
           o[:name] = text
         end
-        Address.maybe_make_one("#{@nses[:cac]}:PostalAddress", n) do |a|
+        maybe_find_one_convert(:make_address, party_el, "#{ns(party_el, :cac)}:PostalAddress") do |a|
           o[:address] = a
         end
-        maybe_find_one("#{@nses[:cac]}:PartyLegalEntity", n) do |len|
-          # ignoring: cbc:CompanyID
-          o[:legal] = OpenStruct.new(
-            {}.tap do |o|
-              maybe_find_one_text("#{@nses[:cbc]}:RegistrationName", len) do |text|
-                o[:name] = text
-              end
-              Address.maybe_make_one("#{@nses[:cac]}:RegistrationAddress", len) do |a|
-                o[:address] = a
-              end
-            end)
+        maybe_find_one_convert(:make_legal, party_el, "#{ns(party_el, :cac)}:PartyLegalEntity") do |l|
+          o[:legal] = l
         end
-        Contact.maybe_make_one("#{@nses[:cac]}:Contact", n) do |c|
-          o[:contact] = c
+        maybe_find_one_convert(:make_contact, party_el, "#{ns(party_el, :cac)}:Contact") do |contact|
+          o[:contact] = contact
         end
-        Person.maybe_make_one("#{@nses[:cac]}:Person", n) do |pr|
-          o[:person] = pr
+        maybe_find_one_convert(:make_person, party_el, "#{ns(party_el, :cac)}:Person") do |person|
+          o[:person] = person
         end
-      end)
-  end
-end
-
-class Address < NodeWrapper
-    # cbc:ID, AddressTypeCode, AddressFormatCode are ignored
-  def city
-    @city ||= maybe_find_one_text("#{@nses[:cbc]}:CityName")
-  end
-
-  def country
-    # doesn't verify the list
-    @country ||= maybe_find_one_country("#{@nses[:cac]}:Country/#{@nses[:cbc]}:IdentificationCode")
-  end
-end
-
-class Contact < NodeWrapper
-  def telephone
-    @telephone ||= maybe_find_one_text("#{@nses[:cbc]}:Telephone")
-  end
-
-  def fax
-    @fax ||= maybe_find_one_text("#{@nses[:cbc]}:Telefax")
-  end
-
-  def email
-    @email ||= maybe_find_one_text("#{@nses[:cbc]}:ElectronicMail")
-  end
-end
-
-class Person < NodeWrapper
-  def name
-    @name ||= OpenStruct.new(
+      end
+    end
+    
+    def make_address(n)
       {}.tap do |o|
-        maybe_find_one_text("#{@nses[:cbc]}:FirstName") do |text|
-          o[:first] = text
+        maybe_find_one_text(n, "#{ns(n, :cbc)}:CityName") do |text|
+          o[:city] = text
         end
-        maybe_find_one_text("#{@nses[:cbc]}:FamilyName") do |text|
-          o[:family] = [text]
+        maybe_find_one_text(n, "#{ns(n, :cac)}:Country/#{ns(n, :cbc)}:IdentificationCode") do |text|
+          o[:country_code] = text
         end
-        maybe_find_one_text("#{@nses[:cbc]}:OtherName") do |text|
-          o[:family] << text
-        end
-        maybe_find_one_text("#{@nses[:cbc]}:MiddleName") do |text|
-          o[:middle] = [text]
-        end
-      end)
-  end
+      end
+    end
 
-  def title
-    @title ||= maybe_find_one_text("#{@nses[:cbc]}:JobTitle")
+    def make_legal(n)
+      # not handling cbc:CompanyID
+      {}.tap do |o|
+        maybe_find_one_text(n, "#{ns(n, :cbc)}:RegistrationName") do |text|
+          o[:name] = text
+        end
+        maybe_find_one_convert(:make_address, n, "#{ns(n, :cac)}:RegistrationAddress") do |a|
+          o[:address] = a
+        end
+      end
+    end
+
+    def make_contact(n)
+      {}.tap do |o|
+        maybe_find_one_text(n, "#{ns(n, :cbc)}:Telephone") do |text|
+          o[:telephone] = text
+        end
+        maybe_find_one_text(n, "#{ns(n, :cbc)}:Telefax") do |text|
+          o[:fax] = text
+        end
+        maybe_find_one_text(n, "#{ns(n, :cbc)}:ElectronicMail") do |text|
+          o[:email] = text
+        end
+      end
+    end
+
+    def make_person(n)
+      @names_set ||= {
+        first:  "#{ns(n, :cbc)}:FirstName",
+        family: "#{ns(n, :cbc)}:FamilyName",
+        other:  "#{ns(n, :cbc)}:OtherName",
+        middle: "#{ns(n, :cbc)}:MiddleName",
+      }
+      {}.tap do |o|
+        maybe_find_set_text(n, @names_set) do |names|
+          o[:name] = names
+        end
+        maybe_find_one_text(n, "#{ns(n, :cbc)}:JobTitle") do |text|
+          o[:title] = text
+        end
+      end
+    end
   end
 end
 
-doc = Nokogiri::XML(open('ubl/documents/UBL-Invoice-2.1-Example.xml')) do |cfg|
-  cfg.noblanks.noent
+include UBL::Parse
+include UBL::Maybes
+include UBL::Invoice
+
+parse('ubl/documents/UBL-Invoice-2.1-Example.xml') do |invoice|
+  p invoice
 end
-
-inv = Invoice.new(doc)
-
-p inv.id
-p inv.issued
-p inv.currency
-p [:period, inv.period.starts, inv.period.ends]
-p [:supplier, inv.parties.supplier.name, inv.parties.supplier.address.city, inv.parties.supplier.address.country.name]
-p [:supplier_legal, inv.parties.supplier.legal.name, inv.parties.supplier.legal.address.country.name]
-p [:supplier_contact, inv.parties.supplier.contact.telephone, inv.parties.supplier.contact.email]
-p [:supplier_person, inv.parties.supplier.person.name, inv.parties.supplier.person.title]
-p [:customer, inv.parties.customer.name, inv.parties.customer.address.city, inv.parties.customer.address.country.name]
-p [:customer_legal, inv.parties.customer.legal.name, inv.parties.customer.legal.address.country.name]
-p [:customer_contact, inv.parties.customer.contact.telephone, inv.parties.customer.contact.email]
-p [:customer_person, inv.parties.customer.person.name, inv.parties.customer.person.title]
-p [:payer, inv.parties.payer.name]
