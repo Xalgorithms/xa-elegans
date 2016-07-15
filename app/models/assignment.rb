31202515284e441090641803c78865c0472ecac0
Don Kelly <karfai@gmail.com>
2016-07-15 00:42:40 -0400

    JSON formatting for Android

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,12 @@
 class Assignment < ActiveRecord::Base
   belongs_to :invocation
   belongs_to :parameter
+
+  def content
+    JSON.parse(actual)
+  end
+
+  def name
+    parameter.name
+  end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,28 @@
 class Invoice < ActiveRecord::Base
   belongs_to :account
+  has_many :invocations, through: :account
+
+  def as_json(opts = {})
+    lopts = {
+      only: [:id, :effective],
+      include: {
+        account: {
+          only: [:id, :name]
+        },
+        invocations: {
+          only: [:id],
+          include: {
+            rule: {
+              only: [:name],
+            },
+            assignments: {
+              only: [:id],
+              methods: [:content, :name],
+            },
+          },
+        }
+      },
+    }
+    super(opts.merge(lopts))
+  end
 end
