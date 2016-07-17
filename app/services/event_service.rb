75ff0c246ed351c63f1277fddbae5eb97f93ee26
Don Kelly <karfai@gmail.com>
2016-07-17 12:40:26 -0400

    Factor transaction manipulation

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -4,16 +4,23 @@ class EventService
   end
   
   def self.transaction_close(e)
-    if !e.transact
-      e.update_attributes(transact: Transaction.find_by(public_id: e.transaction_public_id))
+    attach_transaction(e) do |tr|
+      tr.close
     end
-    e.transact.close if e.transact
   end
 
   def self.invoice_push(e)
+    attach_transaction(e)
+    InvoiceParseService.parse(e.id)
+  end
+
+  private
+
+  def self.attach_transaction(e, &bl)
     if !e.transact
       e.update_attributes(transact: Transaction.find_by(public_id: e.transaction_public_id))
     end
-    InvoiceParseService.parse(e.id)
+
+    bl.call(e.transact) if bl && e.transact
   end
 end
