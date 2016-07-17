ad10c257dc1843f0747f828680aeeea4afff8dfa
Don Kelly <karfai@gmail.com>
2016-07-16 22:34:57 -0400

    Do not init eu bank rates in test

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,12 +1,14 @@
 bank = EuCentralBank.new
 
-cf = Rails.root.join('tmp', 'cache', 'eu_bank_rates.xml')
-bank.save_rates(cf)
-bank.update_rates(cf)
-
-if !bank.rates_updated_at || bank.rates_updated_at < (Time.now - 1.days)
+if !Rails.env.test?
+  cf = Rails.root.join('tmp', 'cache', 'eu_bank_rates.xml')
   bank.save_rates(cf)
   bank.update_rates(cf)
+
+  if !bank.rates_updated_at || bank.rates_updated_at < (Time.now - 1.days)
+    bank.save_rates(cf)
+    bank.update_rates(cf)
+  end
 end
 
 Money.default_bank = bank
