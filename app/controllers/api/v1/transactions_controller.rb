f4ea022210d9cb1eda4c309d12800110ec120cd1
Don Kelly <karfai@gmail.com>
2016-07-16 22:35:12 -0400

    Clean up TransactionsController

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,19 +4,8 @@ module Api
       # TODO: do this properly
       skip_before_filter  :verify_authenticity_token
 
-      before_filter :maybe_lookup_user, only: [:create, :index]
-      before_filter :maybe_lookup_transactions
-      before_filter :maybe_lookup_transaction, only: [:destroy]
-
-      # def create
-      #   @transaction = Transaction.create(status: Transaction::STATUS_OPEN, user: @user)
-      #   render(json: @transaction)
-      # end
-
-      # def destroy
-      #   @transaction.update_attributes(status: Transaction::STATUS_CLOSED) if @transaction
-      #   render(nothing: true)
-      # end
+      before_filter :maybe_lookup_user, only: [:index]
+      before_filter :maybe_lookup_transactions, only: [:index]
 
       def index
         if @transactions
@@ -41,11 +30,6 @@ module Api
         rescue
           Rails.logger.warn("! Failed lookup (user_id=#{user_id})")
         end
-      end
-
-      def maybe_lookup_transaction
-        transaction_id = params.fetch('id', nil)
-        @transaction = Transaction.find(transaction_id) if transaction_id
       end
     end
   end
