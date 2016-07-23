96fdf0b1e50a86db3682d214b42d47cae8666671
Don Kelly <karfai@gmail.com>
2016-07-23 01:29:49 -0400

    Remove junk from Rule model

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,5 +1,2 @@
 class Rule < ActiveRecord::Base
-  # strange naming b/c :transaction is a reserved method in AR::B
-  belongs_to :transact, class_name: 'Transaction', foreign_key: 'transaction_id'
-  has_many :applied_changes, class_name: 'Change', foreign_key: 'rule_id'
 end
