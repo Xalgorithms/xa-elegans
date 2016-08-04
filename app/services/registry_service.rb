ec7427bb1a69fba13b109b5a4395d6fba1ea3bac
Don Kelly <karfai@gmail.com>
2016-08-03 22:28:07 -0400

    Deal with Registry connection problems

--- app/services/registry_service.rb
+++ app/services/registry_service.rb
@@ -4,13 +4,19 @@ class RegistryService
   def self.sync_rules
     last_sync = SyncAttempt.last
 
-    cl = XA::Registry::Client.new(ENV.fetch('XA_REGISTRY_URL', nil) || Settings::Defaults.registry.url)
-    resp = last_sync ? cl.rules(last_sync.token) : cl.rules
+    begin
+      cl = XA::Registry::Client.new(ENV.fetch('XA_REGISTRY_URL', nil) || Settings::Defaults.registry.url)
 
-    SyncAttempt.create(token: resp['since'])
-
-    resp['rules'].map do |ref|
-      Rule.create(reference: ref, public_id: UUID.generate)
+      resp = last_sync ? cl.rules(last_sync.token) : cl.rules
+      
+      SyncAttempt.create(token: resp['since'])
+      
+      resp['rules'].map do |ref|
+        Rule.create(reference: ref, public_id: UUID.generate)
+      end
+    rescue
+      Rails.logger.error("TODO_fix: failed to create client")
     end
+
   end
 end
