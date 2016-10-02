322d5f8056a7823c02a3ad360e8d4cd599bde3a1
Don Kelly <karfai@gmail.com>
2016-10-02 14:59:56 -0400

    Change PG port

--- start_containers.sh
+++ start_containers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name local-postgres -e POSTGRES_PASSWORD=password -p "5432:5432" -d postgres:9.5
+docker run --name local-postgres -e POSTGRES_PASSWORD=password -p "5432:8888" -d postgres:9.5
