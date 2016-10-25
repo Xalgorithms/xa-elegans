22723e6e2073e7ddaea4b98e3b0baf9a3e311613
Don Kelly <karfai@gmail.com>
2016-10-25 12:56:37 -0400

    Factor the settings into something dynamic

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -2,40 +2,31 @@
   function init() {
     console.log('settings: init');
 
-    var tradeshift_vm = {
-    };
-
-    var general_vm = {
-    };
-
     var page_vm = {
-      section: ko.observable('tradeshift')
+      section: ko.observable(default_section),
+      vms: {}
     };
 
+    _.each(sections, function (name) {
+      var is_name = 'is_' + name;
+
+      page_vm.vms[name] = {};
+      page_vm[is_name] = ko.computed(function () {
+	return page_vm.section() === name;
+      });
+      page_vm[name + '_active'] = ko.computed(function () {
+	return page_vm[is_name]() ? 'active' : '';
+      });
+    });
+    
     page_vm.section_clicked = function (vm, e) {
       var target = $(e.currentTarget).data('target');
       page_vm.section(target);
     };
     
-    page_vm.is_general = ko.computed(function () {
-      return page_vm.section() == 'general';
-    });
-    page_vm.general_active = ko.computed(function () {
-	return page_vm.is_general() ? 'active' : '';
-    });
-    
-    page_vm.is_tradeshift = ko.computed(function () {
-      return page_vm.section() == 'tradeshift';
-    });
-    page_vm.tradeshift_active = ko.computed(function () {
-	return page_vm.is_tradeshift() ? 'active' : '';
-    });
-
-    applyManyBindings({
-      'page': page_vm,
-      'tradeshift' : tradeshift_vm,
-      'general' : general_vm
-    });
+    applyManyBindings(
+      _.extend(page_vm.vms, { page: page_vm })
+    );
   }
 
   init_on_page('settings', init);
--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,6 @@
 class SettingsController < ApplicationController
   def index
+    @sections = Settings::Defaults.settings['sections'].map(&:to_sym)
+    @default_section = Settings::Defaults.settings['default_section']
   end
 end
