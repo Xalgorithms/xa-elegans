ec2d451c7b44fa0428a6c8ccc405ce551a1e24f9
Don Kelly <karfai@gmail.com>
2016-10-25 20:19:28 -0400

    Add user_id to settings page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,5 +2,6 @@ class SettingsController < ApplicationController
   def index
     @sections = Settings::Defaults.settings['sections'].map(&:to_sym)
     @default_section = Settings::Defaults.settings['default_section']
+    @user_id = current_user.public_id
   end
 end
