b0c6451fa1951b23427e0078324c114b33ad0465
Don Kelly <karfai@gmail.com>
2016-12-11 16:26:11 -0500

    Remove vestiges of old naming

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 Dotenv::Railtie.load
 
-module Geghard
+module Lichen
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
