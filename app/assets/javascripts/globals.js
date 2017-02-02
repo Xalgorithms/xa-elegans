b82f805c232f008daf494371459e2bfa8fb33848
Don Kelly <karfai@gmail.com>
2017-02-02 00:29:29 -0500

    Fix JS string formatting incompatibility

--- app/assets/javascripts/globals.js
+++ app/assets/javascripts/globals.js
@@ -1,15 +1,18 @@
 var pages = { };
 
 function define_on_page(name, action, init_fn) {
-  pages = _.set(pages, `${name}_${action}`, init_fn);
+  var k = name + '_' + action;
+  pages = _.set(pages, k, init_fn);
 }
 
 function init_on_page(name, action) {
-  var init_fn = _.get(pages, `${name}_${action}`, null);
+  var k = name + '_' + action;
+  var init_fn = _.get(pages, k, null);
   if (init_fn) {
-    console.log(`init: ${name}/${action}`);
+    console.log('init: ' + name + '/' + action);
     init_fn();
   } else {
-    console.log(`missing: ${name}/${action}`);
+    console.log('missing: ' + name + '/' + action);
   }
 }
+
