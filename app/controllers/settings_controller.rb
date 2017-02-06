286b8903b40396bd8d3e12981e62a0dd78768e72
Don Kelly <karfai@gmail.com>
2017-02-06 00:37:44 -0500

    Better derivation of last_sync

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,5 +4,6 @@ class SettingsController < ApplicationController
     @rules = RuleSerializer.many(Rule.all)
     @default_section = Settings::Defaults.settings['default_section']
     @user_id = current_user.public_id
+    @last_sync = SyncAttempt.any? ? SyncAttempt.last.created_at : 'never'
   end
 end
